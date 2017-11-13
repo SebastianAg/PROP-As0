940ebe576ff6960ae12076582ebfef77d31c0589
Freddan962 <fredde.sander@gmail.com>
2017-11-13 16:08:58 +0100

    Cleanup

--- Program.java
+++ Program.java
@@ -15,43 +15,6 @@ public class Program {
 		FileOutputStream stream = null;
 		OutputStreamWriter writer = null;
 
-
-		try {
-			try {
-				//if (args.length < 2)
-				//	throw new Exception("Incorrect number of parameters to program.");
-
-				inputFileName = args[0];
-				outputFileName = args[1];
-
-				parser = new Parser();
-				parser.open(inputFileName);
-				root = parser.parse();
-				builder = new StringBuilder();
-				builder.append("PARSE TREE:\n");
-
-				root.buildString(builder, 0);
-				builder.append("\nEVALUATION:\n");
-				builder.append(root.evaluate(null));
-
-				stream = new FileOutputStream(outputFileName);
-				writer = new OutputStreamWriter(stream);
-				writer.write(builder.toString());
-			} catch (IOException | TokenizerException | ParserException exception) {
-				System.out.println("EXCEPTION: " + exception);
-			} finally {
-				if (parser != null)
-					parser.close();
-				if (writer != null)
-					writer.close();
-				if (stream != null)
-					stream.close();
-			}
-		} catch (IOException exception)
-		{
-			System.out.println("EXCEPTION: " + exception);
-		}
-		/*
 		try {
 			try {
 				if (args.length < 2)
@@ -87,6 +50,6 @@ public class Program {
 		}
 		catch (Exception exception) {
 			System.out.println("EXCEPTION: " + exception);
-		}*/
+		}
 	}
 }
